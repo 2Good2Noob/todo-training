1ffefb3e9332794fc369872b84eea2835c6eae7d
2Good2Noob <artur76321@gmail.com>
2022-03-09 18:42:25 +0100

    Dodanie nawigacji

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './pages/home.page';
 import { HomePageModule } from './pages/home.page-module';
+import { AboutusPageModule } from './pages/aboutus.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'about-us', 
+        loadChildren: () => AboutusPageModule
+      }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), HomePageModule],
--- src/app/pages/aboutus.page-module.ts
+++ src/app/pages/aboutus.page-module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HomePage } from './home.page';
+import { AboutusPage } from './aboutus.page';
 
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
         {
           path: '',
-          component: HomePage,
+          component: AboutusPage,
         }
       ])],
-  	declarations: [HomePage ],
+  	declarations: [AboutusPage],
   	providers: [],
   	exports: [] })
-export class HomePageModule {
+export class AboutusPageModule {
 }
